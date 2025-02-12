cabacaa4df7cd9496be971e64dc008df2b44c6c9
Dennis Ciccone <dennisciccone02@gmail.com>
2025-02-12 22:56:46 +0100

    Svolto l'esercizio Interfacce

---
+++ src/Interfacce/GestioneFigureGeometriche.java
@@ -0,0 +1,24 @@
+/* Scrivere un programma che contenga una interfaccia Forma
+ed un metodo chiamato calcolaArea().
+Crea poi due sottoclassi Rettangolo e Triangolo che implementano Forma
+ed implementano il metodo per il calcolo dell'area.
+ */
+
+
+
+package Interfacce;
+
+public class GestioneFigureGeometriche {
+    public static void main(String[] args) {
+        Rettangolo rettangolo = new Rettangolo(7, 14);
+        Triangolo triangolo = new Triangolo(3, 9);
+
+        System.out.println(rettangolo + "\nArea del rettangolo: " + rettangolo.calcolaArea() );
+        System.out.println(triangolo + "\nArea del triangolo: " + triangolo.calcolaArea() );
+
+
+
+
+
+    }
+}
---
+++ src/Interfacce/Triangolo.java
@@ -0,0 +1,24 @@
+package Interfacce;
+
+public class Triangolo implements IForma {
+    private double base;
+    private double altezza;
+
+    public Triangolo(double base, double altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    @Override
+    public double calcolaArea() {
+        return (base * altezza) / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangolo{" +
+                "base=" + base +
+                ", altezza=" + altezza +
+                '}';
+    }
+}
