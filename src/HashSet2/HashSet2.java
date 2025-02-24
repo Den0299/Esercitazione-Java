0e0351d14a6ed8e8527ffbfbf52b2794898223c4
Dennis Ciccone <dennisciccone02@gmail.com>
2025-02-24 22:39:53 +0100

    Spostati i file HashSet2 e HashSet3 nelle rispettive cartelle

--- src/HashSet2/HashSet2.java
+++ src/HashSet2/HashSet2.java
@@ -1,4 +1,4 @@
-/* Scrivere una funzione che restituisca un HashSet riempito
+package HashSet2;/* Scrivere una funzione che restituisca un HashSet riempito
 Creare un oggetto dello stesso tipo inserito nell'HashSet e popolarlo
 Verificare che l' elemento sia parte del Set e stampare il risultato */
 
